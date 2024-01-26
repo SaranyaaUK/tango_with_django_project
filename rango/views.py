8ba9b02680fab60534dcbc1a2e71e1af2750d170
SaranyaLakshmi ArulJayaKumar <saranyaajk95@outlook.com>
2024-01-26 00:07:29 +0000

    Chapter 4 - Templates, static files and media files hosting

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(respone):
-    return HttpResponse("Rango says hey there partner!")
+def index(request):
+    # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template as its context.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function, the first paramter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+    context_dict = {'boldmessage': "This tutorial has been put together by " , 'emphasismessage': "Saranya Arul"}
+
+    return render(request, 'rango/about.html', context=context_dict)
